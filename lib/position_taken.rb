a0e29cc0ae955512aa75bce8bb4adee07f7d820e
Kristyn Little <kristynlittle@frontier.com>
2021-01-03 20:11:07 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,4 @@
-#board = [" "," "," "," "," "," "," "," "," "]
+board = [" "," "," "," "," "," "," "," "," "]
 
  def position_taken? (board, index)
    if position " " == false || position "" == false
